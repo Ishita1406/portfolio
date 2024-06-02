17ab6ad7a9a27e75517f8e28108a969b037e716a
Ishita <ishita.23bcs10188@ms.sst.scaler.com>
2024-06-03 00:15:45 +0530

    vercel link

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -7,7 +7,7 @@ import { mdiLinkedin } from "@mdi/js";
 import { mdiGoogle } from "@mdi/js";
 import Footer from "../../Components/Footer";
 import './ContactPage.css'
-import prof from '/home/ishita/portfolio/src/Components/assets/profile.jpeg';
+import prof from '/home/ishita/portfolio/src/assets/profile.jpeg';
 
 function ContactPage () {
     return (
